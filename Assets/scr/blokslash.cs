d936a149ed721658ff7d51b2563e63c5a585581c
Vladimir Shperling <vladimirshperling228336@pentagon.com>
2020-10-22 17:11:54 +0700

    eat food and heal

---
+++ Assets/scr/blokslash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blokslash : MonoBehaviour
+{
+    public GameObject cub, slcub,cuboriginal;
+    public float speed;
+    
+
+    
+    void Update()
+    {
+        cuboriginal.transform.Translate(new Vector3(0,0,-1)*speed*Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        cub.SetActive(false);
+        slcub.SetActive(true);
+    }
+}
